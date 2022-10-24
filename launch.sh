07e911f1d5bad1d55beeb0405907da8cd56e0100
LukevLuijn <luke.vanluijn@home.nl>
2022-10-24 19:55:17 +0200

    edit to the launch script

--- launch.sh
+++ launch.sh
@@ -1,19 +1,24 @@
 #!/bin/bash
 
-RQT_CONFIG="config/rqt_config.perspective"
-RVIZ_CONFIG="config/rviz_config.rviz"
+LAUNCH_RQT="rqt --perspective-file config/rqt_config.perspective"
+LAUNCH_RVIZ="rviz2 --display-config config/rviz_config.rviz"
 
-export LC_NUMERIC="en_US.UTF-8"
+POS_Y=10
+run_command ()
+{
+  gnome-terminal --geometry 50x7+10+${POS_Y} -- sh -c "bash -c \"ls && ${1}\" "
+  POS_Y=$(($POS_Y + 200))
+}
 
-# initialize ros environment
-. install/setup.bash
-# launch rqt plot for sim/cup/speed topic with saved perspective.
-gnome-terminal -- sh -c "bash -c \"rqt --perspective-file ${RQT_CONFIG}\" "
-# launch rviz with saved config.
-gnome-terminal -- sh -c "bash -c \"rviz2 --display-config ${RVIZ_CONFIG}\" "
-# run nodes with launch file
-ros2 launch robot_simulation robot_simulation_launch.py # CUP node
+export LC_NUMERIC="en_US.UTF-8" # export to make sure
+. install/setup.bash # initialize ros environment
+
+run_command "${LAUNCH_RQT}" # Launch RQT
+run_command "${LAUNCH_RVIZ}" # Launch RVIZ
+
+ros2 launch robot_simulation robot_simulation_launch.py # Launch simulation
 
 exit
+
 
 
